4cec24af3d550b9ead95687991b5b2c9a6cedaa5
Matheus Victor <matheus.nunes.academy@gmail.com>
2026-07-21 01:00:15 -0400

    Desafio técnico parte 1 - ASC SAC

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,43 @@
+describe('Login', () => {
+  it('Deve realizar o criar login com sucesso', () => {
+    cy.start()
+    cy.goTo('a', 'Signup / Login')
+//criar um perfil de usuário manual os dados
+    cy.get('[data-qa="signup-name"]').type("Matheus")
+    cy.get('[data-qa="signup-email"]').type("Matheus1.@teste.com")
+    cy.get('[data-qa="signup-button"]').click()
+    cy.get('#id_gender1').click()
+    cy.get('[data-qa="password"]').type("Katana@123")
+    cy.get('[data-qa="days"]').select("9")
+    cy.get('[data-qa="months"]').select("June")
+    cy.get('[data-qa="years"]').select("1999")
+    cy.get('[data-qa="first_name"]').type("Matheus")
+    cy.get('[data-qa="last_name"]').type("Nunes")
+    cy.get('[data-qa="address"]').type("Rua itajaruna, n.48")
+    cy.get('[data-qa="country"]').select("Canada")
+    cy.get('[data-qa="state"]').type("Canada")
+    cy.get('[data-qa="city"]').type("Toronto")
+    cy.get('[data-qa="zipcode"]').type("13458-815")
+    cy.get('[data-qa="mobile_number"]').type("9737456283")
+    cy.get('[data-qa="create-account"]').click()
+    cy.get('[data-qa="continue-button"]').click()
+
+  })
+//login com um usuário já criado
+  it.only('login com sucesso', ()=> {
+    cy.start()
+    cy.goTo('a', 'Signup / Login')
+    cy.submitLoginForm('Matheus1.@teste.com','Katana@123')
+  })
+//tentativa de login com usuário valido e senha invalida + validação de mensagem
+  it('Login com senha invalida', ()=> {
+    cy.start()
+    cy.goTo('a', 'Signup / Login')
+    cy.submitLoginForm('Matheus1.@teste.com','Katana124')
+     
+    cy.contains('Your email or password is incorrect!')
+      .should('be.visible')
+      .and('have.text', 'Your email or password is incorrect!')
+  })
+
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,44 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('start', () => {
+    cy.viewport(1440, 900)
+    cy.visit('https://automationexercise.com')
+
+})
+
+Cypress.Commands.add('goTo', (button, pagefile)=> {
+      cy.contains(button, pagefile)
+      .should('be.visible')
+      .click()
+
+})
+
+Cypress.Commands.add('submitLoginForm', (email, password) => {
+    cy.get('[data-qa="login-email"]').type(email)
+    cy.get('[data-qa="login-password"]').type(password)
+    cy.get('[data-qa="login-button"]').click()
+})
